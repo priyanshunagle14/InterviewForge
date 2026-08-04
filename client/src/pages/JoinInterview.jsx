7716f6ada6ed25e20428f92fe3b9980e89106510
priyanshunagle14 <priyanshunagle2020@gmail.com>
2026-08-04 13:54:17 +0530

    fix: import missing API_URL in JoinInterview.jsx

--- client/src/pages/JoinInterview.jsx
+++ client/src/pages/JoinInterview.jsx
@@ -4,6 +4,7 @@ import Button from "../components/Common/Button";
 import Card from "../components/Common/Card";
 import BackButton from "../components/Common/BackButton";
 import { useAuth } from "../context/AuthContext";
+import { API_URL } from "../config";
 
 export default function JoinInterview() {
     const [interviewId, setInterviewId] = useState("");
